f2dfed50fe297f34104c4d2ead27d2407c354035
Leo Chen <leo@harmony.one>
2020-02-21 21:55:56 +0000

    signal handler testing
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ go/os/signal.go
@@ -0,0 +1,44 @@
+package main
+
+// this test program is to test signal handler in golang and gracefully shutdown the program
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func shutdown() {
+	fmt.Fprintf(os.Stderr, "%d exiting ...\n", os.Getpid())
+	os.Exit(0)
+}
+
+func main() {
+	// Prepare for graceful shutdown from os signals
+	osSignal := make(chan os.Signal)
+	signal.Notify(osSignal, os.Interrupt, os.Kill, syscall.SIGTERM)
+	go func() {
+		for {
+			select {
+			case sig := <-osSignal:
+				if sig == os.Kill || sig == syscall.SIGTERM {
+					fmt.Fprintf(os.Stderr, "Got %s signal. Gracefully shutting down...\n", sig)
+					shutdown()
+				}
+				if sig == os.Interrupt {
+					fmt.Fprintf(os.Stderr, "Got %s signal. Dumping state to DB...\n", sig)
+					shutdown()
+				}
+			}
+		}
+	}()
+
+   t := 0
+	for {
+		fmt.Println("sleeping ...", t)
+		time.Sleep(5 * time.Second)
+      t++
+	}
+}
