19f06c87a9c5e581ecfe913a16b09e07cf9b6c88
Leo Chen <leo@harmony.one>
2020-04-25 06:57:49 +0000

    [signal] add a coredump triggered by SIGUSR1
    Signed-off-by: Leo Chen <leo@harmony.one>

--- go/os/signal.go
+++ go/os/signal.go
@@ -18,7 +18,7 @@ func shutdown() {
 func main() {
 	// Prepare for graceful shutdown from os signals
 	osSignal := make(chan os.Signal)
-	signal.Notify(osSignal, os.Interrupt, os.Kill, syscall.SIGTERM)
+	signal.Notify(osSignal, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGUSR1)
 	go func() {
 		for {
 			select {
@@ -31,14 +31,20 @@ func main() {
 					fmt.Fprintf(os.Stderr, "Got %s signal. Dumping state to DB...\n", sig)
 					shutdown()
 				}
+				if sig == syscall.SIGUSR1 {
+					core := make([]int, 0)
+					fmt.Printf("coredump: %s", core[0])
+					// won't reach here
+					shutdown()
+				}
 			}
 		}
 	}()
 
-   t := 0
+	t := 0
 	for {
-		fmt.Println("sleeping ...", t)
+		fmt.Println("sleeping ..., waiting for signal", t)
 		time.Sleep(5 * time.Second)
-      t++
+		t++
 	}
 }
